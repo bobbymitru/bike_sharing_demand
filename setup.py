de45003ed28cf4d2e96952d236d999af83ddfb22
Robert Mitru <robert.mitru.business@gmail.com>
2023-10-23 18:49:27 +0300

    Created a basic project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The data of bike rental demand in the Capital Bikeshare program in Washington, D.C. Bike sharing and rental systems are in general good sources of information',
+    author='Robert Mitru',
+    license='MIT',
+)
